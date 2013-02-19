5875f6c80739994cbf58f553f9fdb94fded27358
Jaroslaw Hirniak <j@hirniak.info>
2013-02-19 16:09:23 +0000

    Jarek - Extra pizza (scripts for labelling and some mock-up of labels + API for Yelp)

---
+++ data_mining/extract_labels.py
@@ -0,0 +1,24 @@
+import csv
+#from labels import sports
+
+strip_non_alphas = lambda s : ''.join( x for x in s if x.isalpha() or x == ' ' )    
+remove_unwanted = lambda s, unwanted : ' '.join( x for x in s.split(' ') if x not in unwanted )
+fit_labels = lambda source, target : [ l for l in source if target.index(l) ]
+
+def extract_labels(csvfile, label, delimiter=',', quotechar='"'):
+    labels = set()
+    csvdict = csv.DictReader(csvfile, delimiter=delimiter,  quotechar=quotechar)
+    for d in csvdict:
+        for l in d[label].split('\n'):            
+            if l != '':
+                l = l.lower()
+                l = strip_non_alphas(l)
+                l = remove_unwanted(l, ['including'])
+                labels.add(l)
+    return labels
+
+# csvfile = open('sport_and_recreational_facilities.csv')
+# print extract_labels(csvfile, 'Activities')
+
+csvfile = open('play_areas.csv')
+print extract_labels(csvfile, 'Play facilities')
---
+++ data_mining/labels.py
@@ -0,0 +1,9 @@
+sport = set(['skating', 'golf', 'coached activities', 'dance', 'judo', 'water sports', 'badminton', 'gymnastics', 'softplay', 'certificated courses', 'bowling', 'abseiling', 'pitch and putt', 'basketball', 'cycling', 'canoeing', 'aerobic', 'fencing', 'tennis', 'coaching', 'orienteering', 'hockey', 'croquet', 'powerboat', 'swim', 'cricket', 'gym', 'football', 'hillwalking', 'table tennis', 'dinghy cruising', 'bouldering', 'american football', 'climbing', 'bmx', 'short tennis', 'school holiday activities', 'turkish baths', 'equipment hire', 'rugby', 'sailing', 'racing', 'aquafit', 'kayaking', 'skateboard', 'volleyball', 'aerial assault ', 'older adult activities', 'squash', 'fitness', 'athletics', 'biking', 'massage'])
+
+playarea = [ ['ballcourt', 'ball court'], , 'skateboard', 'graffiti',  'bmx', ['basketball', 'basket ball'], 'tennis', 'bike' ]
+playarea_items = [ 'swing', 'slide', 'climbing', 'roundabout', 'play area', 'bike', 'pump track', 'ball end', 'graffiti wall', 'table tennis table', 'fenced court', 'net', 'balancing beam', 'bike track', 'seating' ]
+
+age = ['teen','toddler', 'junior']
+
+# museums and galleries, just list of museums labelled as museums and galleries?
+# parks and green spaces labelled as parks?
