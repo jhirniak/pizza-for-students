e9fb3fbc055de0b85e2b918e532e808713d8bebe
bvborisov <bilyan.borisov@gmail.com>
2013-02-21 20:12:33 +0200

    adding new django connection

--- mmd/mmd/urls.py
+++ mmd/mmd/urls.py
@@ -8,10 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', welcome),
     url(r'^make/$', make),
-
-    
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': '/home/bilyan/ilw/pizza-for-students/html/'}), #this is to server static files
     # url(r'^mmd/', include('mmd.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
